6137dc19d1c36849714dae6e539af386aa3038e4
Yifan Lu <115194735+yifan-lu-300@users.noreply.github.com>
2024-03-10 19:31:47 +0000

    Modified sample selection to also remove respondents not eligible for the main self-completion questionnaire

--- src/1_sample_selection.py
+++ src/1_sample_selection.py
@@ -9,13 +9,14 @@ covid_path = Path('1_sample_selection.py').resolve().parents[2] / 'Data' / 'elsa
 derived_path = Path('1_sample_selection.py').resolve().parents[2] / 'Data' / 'derived'
 
 # Read data
-main_var = ['idauniq',
+main_var = ['idauniq', 'w8scout',
             'scprt', 'chinhh', 'cafam', 'w8nssec5', # controls
             'cahha', # sample selection
             'hehelf', # self-reported health
             'psceda', 'pscedb', 'pscedc', 'pscedd', 'pscede', 'pscedf', 'pscedg', 'pscedh', # ces-d
-            'hobb', 'scint', 'scind96',
-            'scinaem', 'scinacl', 'scinaed', 'scinabk', 'scinash', 'scinasl', 'scinasn', 'scinact', 'scinanw', 'scinast', 'scinagm', 'scinajb', 'scinaps', 'scina95', 'scina96',
+            'hobb', 'scint',
+            'scinddt', 'scindlt', 'scindtb', 'scindph', 'scind95', 'scind96', # devices used to access internet
+            'scinaem', 'scinacl', 'scinaed', 'scinabk', 'scinash', 'scinasl', 'scinasn', 'scinact', 'scinanw', 'scinast', 'scinagm', 'scinajb', 'scinaps', 'scina95', 'scina96', # internet activities
             'hedasbp', 'hedimbp', 'hedasch', 'hedimch', 'hedacdi', 'hedimdi', # three cardiovascular diseases
             'hedbsas', 'hedibas', 'hedbsar', 'hedibar', 'hedbsca', 'hedibca'] + \
     [f'cafam{number}' for number in range(2, 26)] # whether person who helps lives in same household as respondent
@@ -26,7 +27,7 @@ finance_var = ['idauniq', 'yq10_bu_s', 'totwq10_bu_s']
 finance_8 = pd.read_table(main_path / 'wave_8_elsa_financial_dvs_eul_v1.tab',
                           usecols=finance_var)
 
-ifs_var = ['idauniq', 'wgt',
+ifs_var = ['idauniq',
            'age', 'sex', 'nonwhite', 'marstat', 'qual2', 'qual3', 'ndepriv', 'llsill', # controls
            'wselfd', 'inst', # sample selection
            'srh_hrs', 'cesd_sc']
@@ -38,17 +39,20 @@ full_8 = main_8.merge(finance_8, on='idauniq', how='left') \
                 .merge(ifs_8, on='idauniq', how='left')
 
 ########## Sample selection
-full_8['inst'].value_counts(dropna=False) # remove respondents in institution (-58)
+full_8['w8scout'].value_counts(dropna=False) # remove respondents not eligible for the main self-completion questionnaire (-1223)
+full_8 = full_8.loc[full_8['w8scout'] == 1, :]
+
+full_8['inst'].value_counts(dropna=False) # remove respondents in institution (-2)
 full_8 = full_8.loc[full_8['inst'] == 0, :]
 
-full_8['cahha'].value_counts(dropna=False) # remove respondents living with formal care (e.g. home care worker) (-160)
+full_8['cahha'].value_counts(dropna=False) # remove respondents living with formal care (e.g. home care worker) (-85)
 full_8 = full_8.loc[full_8['cahha'] == -1, :]
 
 full_8['cafam'].value_counts(dropna=False)
 help_list = ['cafam'] + [f'cafam{number}' for number in range(2, 26)]
-full_8 = full_8.loc[~(full_8[help_list] == 1).any(axis=1), :] # remove respondents living with informal care (e.g. son) (-815)
+full_8 = full_8.loc[~((full_8[help_list] == 1).any(axis=1)), :] # remove respondents living with informal care (e.g. son) (-674)
 
-full_8['wselfd'].value_counts(dropna=False) # select respondents who are retired
+full_8['wselfd'].value_counts(dropna=False) # select respondents who are retired (-2278)
 full_8 = full_8.loc[full_8['wselfd'] == 3, :]
 
 ########## Save data
@@ -58,4 +62,4 @@ full_8.to_csv(derived_path / 'wave_8.csv', index=False)
 full_8['hobb'].value_counts()
 
 full_8['scprt'].value_counts() # 2803 lives with partner
-full_8['chinhh'].value_counts() # 549 do not live with children
+full_8['chinhh'].value_counts() # 474 do not live with children
