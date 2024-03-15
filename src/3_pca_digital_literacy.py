5dd9925ace2ae8688f32fb56a370715f756e0343
Yifan Lu <115194735+yifan-lu-300@users.noreply.github.com>
2024-03-15 02:35:20 +0000

    Improved the format of both the IPTW table and the pc1_loadings table

--- src/3_pca_digital_literacy.py
+++ src/3_pca_digital_literacy.py
@@ -24,7 +24,7 @@ digital_var = ['idauniq',
 
 # Remove NAs
 main_10['idauniq'].isna().sum()  # no NAs in idauniq
-digital_10 = main_10[digital_var].dropna() # 54 dropped
+digital_10 = main_10[digital_var].dropna()  # 54 dropped
 
 # Scale the variables before performing PCA
 scaler = StandardScaler(with_std=True, with_mean=True)
@@ -35,14 +35,21 @@ pca_digital = PCA()
 pca_digital.fit(digital_10_scaled)
 
 # Loadings
-pca_loadings = pd.DataFrame(pca_digital.components_)  # each row is a principal component, and columns are corresponding variable loadings
+pca_loadings = pd.DataFrame(
+    pca_digital.components_)  # each row is a principal component, and columns are corresponding variable loadings
 
 ########## PC1 loadings
-pc1_loadings = pd.DataFrame({'Variable': ['Frequency of using the Internet',
-                                          'desktop', 'laptop', 'tablet', 'smartphone', 'other',
-                                          'emails', 'video calls', 'finding information', 'finances', 'shopping', 'selling', 'social networking', 'news', 'TV/radio', 'music', 'games', 'e-books', 'job application', 'government services', 'checking travel times', 'satellite navigation', 'buying public transport tickets', 'booking a taxi', 'finding local amenities', 'controlling household appliances', 'none of the above'],
-                             'Loading': pca_loadings.iloc[0, :] | p(round, 3),
-                             'Description': np.nan})
+pc1_loadings = pd.DataFrame({'Item': ['Frequency of using the Internet',
+                                      'desktop', 'laptop', 'tablet', 'smartphone', 'other',
+                                      'emails', 'video calls', 'finding information', 'finances', 'shopping', 'selling',
+                                      'social networking', 'news', 'TV/radio', 'music', 'games', 'e-books',
+                                      'job application', 'government services', 'checking travel times',
+                                      'satellite navigation', 'buying public transport tickets', 'booking a taxi',
+                                      'finding local amenities', 'controlling household appliances',
+                                      'none of the above'],
+                             'Description': np.nan,
+                             'Loading': pca_loadings.iloc[0, :] | p(round, 3)
+                             })
 
 pc1_loadings.to_latex(index=False, float_format="%.3f") | p(print)
 
@@ -62,8 +69,8 @@ pn.ggsave(pca_screeplot, filename='pca_screeplot.png', path=figure_path, dpi=300
 
 # Extract PCA scores
 pca_scores = pca_digital.transform(digital_10_scaled) | p(pd.DataFrame)
-digital_10['PC1'] = pca_scores.iloc[:, 0].values # smaller values indicate better digital literacy
-digital_10['PC1'].isna().sum() # no NAs
+digital_10['PC1'] = pca_scores.iloc[:, 0].values  # smaller values indicate better digital literacy
+digital_10['PC1'].isna().sum()  # no NAs
 
 # Merge PC1 scores with the main data
 main_10 = main_10.merge(digital_10[['idauniq', 'PC1']], on='idauniq', how='left')
