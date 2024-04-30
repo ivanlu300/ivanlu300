b62f476856a5a54a1f4307e7ffd1d1e961ab1ba1
Yifan Lu <115194735+yifan-lu-300@users.noreply.github.com>
2024-04-30 16:11:08 +0100

    Completed DiD estimation (with regression interaction term) and its table output

--- src/question_2/5_did.py
+++ src/question_2/5_did.py
@@ -43,10 +43,6 @@ sample_did_long = pd.wide_to_long(sample_did,
                                   suffix='\\d+'
                                   ).reset_index()
 
-did_srh = smf.ols('srh ~ group * C(wave)',
-                  data=sample_did_long).fit()
-did_srh.summary()
-
 did_srh = smf.ols(
     'srh ~ group * C(wave) + employ_status + total_income_bu_d + C(marital_status) + memory + numeracy + n_deprived + age + sex_9 + ethnicity_9 + edu_age_9 + C(edu_qual_9)',
     data=sample_did_long).fit()
@@ -93,5 +89,40 @@ did_cesd_b = smf.ols(
 did_cesd_b.summary()
 
 ########## Using a for loop to save results in one table
-models = [did_srh, did_high_bp, did_high_chol, did_diabetes, did_asthma, did_arthritis, did_cancer, did_cesd,
-          did_cesd_b]
+result_table = pd.DataFrame({'Outcome': ['Self-reported health', '', 'High blood pressure', '', 'High cholesterol', '',
+                                         'Diabetes', '', 'Asthma', '', 'Arthritis', '', 'Cancer', '', 'CES-D items', '',
+                                         'CES-D diagnosis', ''],
+                             'DiD': np.nan,
+                             'p_value': np.nan})
+outcome_list = ['srh', 'high_bp', 'high_chol', 'diabetes', 'asthma', 'arthritis', 'cancer', 'cesd', 'cesd_b']
+
+for i in outcome_list:
+    model = smf.ols(
+        f'{i} ~ group * C(wave) + employ_status + total_income_bu_d + C(marital_status) + memory + numeracy + n_deprived + age + sex_9 + ethnicity_9 + edu_age_9 + C(edu_qual_9)',
+        data=sample_did_long).fit()
+
+    result_table.loc[outcome_list.index(i) * 2, 'DiD'] = model.params['group:C(wave)[T.10]']
+    result_table.loc[outcome_list.index(i) * 2 + 1, 'DiD'] = model.tvalues['group:C(wave)[T.10]']
+    result_table.loc[outcome_list.index(i) * 2, 'p_value'] = model.pvalues['group:C(wave)[T.10]']
+
+########## Improve the table format
+result_table['DiD'] = result_table['DiD'] | p(round, 3)
+
+# Add * to indicate statistical significance
+for i in np.arange(0, result_table.shape[0], 2):
+    if abs(result_table.loc[i, 'p_value']) <= 0.001:
+        result_table.loc[i, 'DiD'] = str(result_table.loc[i, 'DiD']) + '***'
+    elif (abs(result_table.loc[i, 'p_value']) <= 0.01) & (abs(result_table.loc[i, 'p_value']) > 0.001):
+        result_table.loc[i, 'DiD'] = str(result_table.loc[i, 'DiD']) + '**'
+    elif (abs(result_table.loc[i, 'p_value']) <= 0.05) & (abs(result_table.loc[i, 'p_value']) > 0.01):
+        result_table.loc[i, 'DiD'] = str(result_table.loc[i, 'DiD']) + '*'
+
+# Add square brackets to t-values
+result_table['DiD'] = result_table['DiD'].astype(str)
+result_table.loc[result_table.index % 2 == 1, 'DiD'] = '[' + result_table['DiD'] + ']'
+
+# Add a description column
+result_table['Description'] = [''] * result_table.shape[0]
+
+########## LaTeX
+result_table[['Outcome', 'Description', 'DiD']].to_latex(index=False, float_format="%.3f") | p(print)
