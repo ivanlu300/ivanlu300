33ee43a9317a10a61f5c596706d63b0c549e2dfe
Yifan Lu <115194735+yifan-lu-300@users.noreply.github.com>
2024-06-05 23:57:27 +0100

    Modified the way to deal with contamination of group assignment for RQ2, following the advice of my supervisor

--- src/question_2/3_pca_digital_literacy.py
+++ src/question_2/3_pca_digital_literacy.py
@@ -18,16 +18,16 @@ table_path = Path().resolve().parents[1] / 'output' / 'table'
 sample = pd.read_csv(derived_path / 'wave_910_cleaned.csv')
 
 ########## Wave 9 PCA
-# Select variables related to digital literacy
+# Select the intersection (i.e., inner join) of the features sets for Wave 9 and Wave 10
 digital_var_9 = ['idauniq',
                  'int_freq_9'] + \
-                ['scinddt', 'scindlt', 'scindtb', 'scindph', 'scind95', 'scind96'] + \
-                ['scinaem', 'scinacl', 'scinaed', 'scinahe', 'scinabk', 'scinash', 'scinasl', 'scinasn', 'scinact',
-                 'scinanw', 'scinast', 'scinagm', 'scinajb', 'scinaps', 'scina95', 'scina96']
+                ['scinddt', 'scindlt', 'scindtb', 'scindph', 'scind95'] + \
+                ['scinaem', 'scinacl', 'scinaed', 'scinabk', 'scinash', 'scinasl', 'scinasn',
+                 'scinanw', 'scinast', 'scinagm', 'scinajb', 'scinaps', 'scina96']
 
 # Remove NAs
 sample['idauniq'].isna().sum()  # no NAs in idauniq
-digital_9 = sample[digital_var_9].dropna()  # N = 3557
+digital_9 = sample[digital_var_9].dropna()  # N = 3603
 
 # Scale the variables before performing PCA
 scaler_9 = StandardScaler(with_std=True, with_mean=True)
@@ -42,13 +42,12 @@ pca_loadings_9 = pd.DataFrame(pca_digital_9.components_)
 # each row is a principal component, and columns are corresponding variable loadings
 
 # PC1 loadings
-pc1_loadings_9 = pd.DataFrame({'Item': ['Frequency of using the Internet',
+pc1_loadings_9 = pd.DataFrame({'Item': ['Frequency of Internet usage',
                                         'desktop', 'laptop', 'tablet', 'smartphone', 'other',
-                                        'do not use any device',
-                                        'emails', 'video calls', 'finding information (learning)',
-                                        'finding information (health)', 'finances', 'shopping', 'selling',
-                                        'social networking', 'creating content', 'news', 'music', 'games',
-                                        'job application', 'government services', 'other online activities', 'no online activities'],
+                                        'emails', 'video calls', 'finding information', 'finances', 'shopping',
+                                        'selling',
+                                        'social networking', 'news', 'music', 'games',
+                                        'job application', 'government services', 'no online activities'],
                                'Description': '',
                                'Loading': pca_loadings_9.iloc[0, :] | p(round, 3)
                                })
@@ -77,6 +76,9 @@ pca_scores_9 = pca_digital_9.transform(digital_9_scaled) | p(pd.DataFrame)
 digital_9['PC1_9'] = pca_scores_9.iloc[:, 0].values  # smaller values indicate better digital literacy
 digital_9['PC1_9'].isna().sum()  # no NAs
 
+# Calculate the standard deviation of PC1
+digital_9['PC1_9'].std()  # 2.15
+
 # Merge PC1 scores with the main data
 sample = sample.merge(digital_9[['idauniq', 'PC1_9']], on='idauniq', how='left')
 sample['PC1_9'].value_counts(dropna=False)
@@ -86,98 +88,101 @@ sample['PC1_b_9'] = np.select(condlist=[sample['PC1_9'] <= sample['PC1_9'].quant
                                         sample['PC1_9'] >= sample['PC1_9'].quantile(0.75)],
                               choicelist=[1, 0],
                               default=np.nan)  # 1 = high digital literacy, 0 = low digital literacy
-sample['PC1_b_9'].value_counts(dropna=False)  # 1 = 891, 0 = 890, NaN = 1822
+sample['PC1_b_9'].value_counts(dropna=False)  # 1 = 891, 0 = 891, NaN = 1821
 
 ########## Wave 10 PCA
-# Select variables related to digital literacy
+# Select the intersection (i.e., inner join) of the features sets for Wave 9 and Wave 10
 digital_var_10 = ['idauniq',
                   'int_freq_10'] + \
                  [f'SCIND0{number}' for number in range(1, 6)] + \
-                 [f'SCINA0{number}' for number in range(1, 10)] + [f'SCINA{number}' for number in range(10, 22)]
+                 ['SCINA01', 'SCINA02', 'SCINA03', 'SCINA04', 'SCINA05', 'SCINA06', 'SCINA07', 'SCINA08'] + \
+                 ['SCINA10', 'SCINA11', 'SCINA13', 'SCINA14', 'SCINA21']
 
 # Remove NAs
 sample['idauniq'].isna().sum()  # no NAs in idauniq
-digital_10 = sample[digital_var_10].dropna()  # N = 3557
+digital_10 = sample[digital_var_10].dropna()  # N = 3566
 
 # Scale the variables before performing PCA
 scaler_10 = StandardScaler(with_std=True, with_mean=True)
 digital_10_scaled = scaler_10.fit_transform(digital_10.iloc[:, 1:])
+#
+# # Perform PCA
+# pca_digital_10 = PCA()
+# pca_digital_10.fit(digital_10_scaled)
+#
+# # Loadings
+# pca_loadings_10 = pd.DataFrame(pca_digital_10.components_)
+# # each row is a principal component, and columns are corresponding variable loadings
+#
+# # PC1 loadings
+# pc1_loadings_10 = pd.DataFrame({'Item': ['Frequency of using the Internet',
+#                                          'desktop', 'laptop', 'tablet', 'smartphone', 'other devices',
+#                                          'emails', 'video calls', 'finding information', 'finances', 'shopping', 'selling',
+#                                          'social networking', 'news', 'TV/radio', 'music', 'games', 'e-books',
+#                                          'job application', 'government services', 'checking travel times',
+#                                          'satellite navigation', 'buying public transport tickets', 'booking a taxi',
+#                                          'finding local amenities', 'controlling household appliances',
+#                                          'no online activities'],
+#                                 'Description': '',
+#                                 'Loading': pca_loadings_10.iloc[0, :] | p(round, 3)
+#                                 })
+#
+# pc1_loadings_10.to_latex(index=False, float_format="%.3f") | p(print)
+#
+# # Explained variance (proportion)
+# pca_pve_10 = pd.DataFrame({'PC': range(1, digital_10_scaled.shape[1] + 1),
+#                            'PVE': pca_digital_10.explained_variance_ratio_})
+#
+# # Screeplot
+# fig_10 = px.scatter(pca_pve_10, x='PC', y='PVE',
+#                     labels={'PC': 'Principal component', 'PVE': 'Proportion of variance explained'})
+# fig_10.add_trace(px.line(pca_pve_10, x='PC', y='PVE').data[0])
+#
+# fig_10.update_layout(
+#     xaxis=dict(tickmode='array', tickvals=list(range(1, digital_10_scaled.shape[1] + 1))),
+#     yaxis=dict(tickmode='array', tickvals=list(np.arange(0, 0.30, 0.05))),
+# )
+#
+# # fig_10.write_image(figure_path / 'pca_screeplot_w10_q2.png')
+# # fig_10.show()
+#
+# # combine the two plots
+# fig = make_subplots(rows=1, cols=2, subplot_titles=('Wave 9', 'Wave 10'))
+#
+# for trace in fig_9.data:
+#     fig.add_trace(trace, row=1, col=1)
+#
+# fig.update_xaxes(title='Principal component', row=1, col=1)
+# fig.update_yaxes(title='Proportion of variance explained', row=1, col=1)
+#
+# for trace in fig_10.data:
+#     fig.add_trace(trace, row=1, col=2)
+#
+# fig.update_xaxes(title='Principal component', row=1, col=2)
+#
+# fig.write_image(figure_path / 'pca_screeplot_q2.png')
+# fig.show()
 
-# Perform PCA
-pca_digital_10 = PCA()
-pca_digital_10.fit(digital_10_scaled)
-
-# Loadings
-pca_loadings_10 = pd.DataFrame(pca_digital_10.components_)
-# each row is a principal component, and columns are corresponding variable loadings
-
-# PC1 loadings
-pc1_loadings_10 = pd.DataFrame({'Item': ['Frequency of using the Internet',
-                                         'desktop', 'laptop', 'tablet', 'smartphone', 'other devices',
-                                         'emails', 'video calls', 'finding information', 'finances', 'shopping', 'selling',
-                                         'social networking', 'news', 'TV/radio', 'music', 'games', 'e-books',
-                                         'job application', 'government services', 'checking travel times',
-                                         'satellite navigation', 'buying public transport tickets', 'booking a taxi',
-                                         'finding local amenities', 'controlling household appliances',
-                                         'no online activities'],
-                                'Description': '',
-                                'Loading': pca_loadings_10.iloc[0, :] | p(round, 3)
-                                })
-
-pc1_loadings_10.to_latex(index=False, float_format="%.3f") | p(print)
-
-# Explained variance (proportion)
-pca_pve_10 = pd.DataFrame({'PC': range(1, digital_10_scaled.shape[1] + 1),
-                           'PVE': pca_digital_10.explained_variance_ratio_})
-
-# Screeplot
-fig_10 = px.scatter(pca_pve_10, x='PC', y='PVE',
-                    labels={'PC': 'Principal component', 'PVE': 'Proportion of variance explained'})
-fig_10.add_trace(px.line(pca_pve_10, x='PC', y='PVE').data[0])
-
-fig_10.update_layout(
-    xaxis=dict(tickmode='array', tickvals=list(range(1, digital_10_scaled.shape[1] + 1))),
-    yaxis=dict(tickmode='array', tickvals=list(np.arange(0, 0.30, 0.05))),
-)
-
-# fig_10.write_image(figure_path / 'pca_screeplot_w10_q2.png')
-# fig_10.show()
-
-# combine the two plots
-fig = make_subplots(rows=1, cols=2, subplot_titles=('Wave 9', 'Wave 10'))
-
-for trace in fig_9.data:
-    fig.add_trace(trace, row=1, col=1)
-
-fig.update_xaxes(title='Principal component', row=1, col=1)
-fig.update_yaxes(title='Proportion of variance explained', row=1, col=1)
-
-for trace in fig_10.data:
-    fig.add_trace(trace, row=1, col=2)
-
-fig.update_xaxes(title='Principal component', row=1, col=2)
-
-fig.write_image(figure_path / 'pca_screeplot_q2.png')
-fig.show()
-
-# Extract PCA scores
-pca_scores_10 = pca_digital_10.transform(digital_10_scaled) | p(pd.DataFrame)
-digital_10['PC1_10'] = pca_scores_10.iloc[:, 0].values  # smaller values indicate better digital literacy
-digital_10['PC1_10'].isna().sum()  # no NAs
+# Use the same loadings from Wave 9 to construct digital literacy index for Wave 10
+digital_10['PC1_10'] = pd.DataFrame(digital_10_scaled).apply(lambda x: sum([a * b for a, b in zip(pca_loadings_9.iloc[0, :], x)]), axis=1)
 
 # Merge PC1 scores with the main data
 sample = sample.merge(digital_10[['idauniq', 'PC1_10']], on='idauniq', how='left')
 sample['PC1_10'].value_counts(dropna=False)
 
-# binary PC1
-sample['PC1_b_10'] = np.select(condlist=[sample['PC1_10'] <= sample['PC1_10'].quantile(0.25),
-                                         sample['PC1_10'] >= sample['PC1_10'].quantile(0.75)],
-                               choicelist=[1, 0],
-                               default=np.nan)  # 1 = high digital literacy, 0 = low digital literacy
-sample['PC1_b_10'].value_counts(dropna=False)  # 1 = 892, 0 = 892, NaN = 1819
+# difference in digital literacy index between Wave 9 and Wave 10
+sample['PC1_diff'] = sample['PC1_10'] - sample['PC1_9']
+
+#
+sample = sample.loc[sample['PC1_b_9'].notnull(), :]
+
+#
+sample = sample.loc[abs(sample['PC1_diff']) < digital_9['PC1_9'].std(), :]
+
+#
+sample['PC1_b_9'].value_counts(dropna=False)
 
 ########## Save data
 sample.to_csv(derived_path / 'wave_910_pca.csv', index=False)
 
 ########## Inspection
-pd.crosstab(sample['PC1_b_9'], sample['PC1_b_10'])
--- src/question_2/5_did.py
+++ src/question_2/5_did.py
@@ -19,34 +19,35 @@ table_path = Path().resolve().parents[1] / 'output' / 'table'
 # Read data
 sample = pd.read_csv(derived_path / 'wave_910_pca.csv')
 
-########## Further sample selection: only respondents who did not change their digital literacy between Wave 9 and 10
-pd.crosstab(sample['PC1_b_9'], sample['PC1_b_10'])
-sample_910 = sample.loc[sample['PC1_b_9'] == sample['PC1_b_10'], :]  # N = 2881
-sample_910['group'] = sample_910['PC1_b_9']
-sample_910['group'].value_counts(dropna=False)  # 1: 565, 0: 658
+# ########## Further sample selection: only respondents who did not change their digital literacy between Wave 9 and 10
+# pd.crosstab(sample['PC1_b_9'], sample['PC1_b_10'])
+# sample_910 = sample.loc[sample['PC1_b_9'] == sample['PC1_b_10'], :]  # N = 2881
+# sample_910['group'] = sample_910['PC1_b_9']
+# sample_910['group'].value_counts(dropna=False)  # 1: 565, 0: 658
 
-sample_did = sample_910[['idauniq', 'group',
-                         'srh_9', 'srh_10',
-                         'high_bp_9', 'high_bp_10', 'high_chol_9', 'high_chol_10', 'diabetes_9', 'diabetes_10',
-                         'asthma_9', 'asthma_10', 'arthritis_9', 'arthritis_10', 'cancer_9', 'cancer_10',
-                         'cesd_9', 'cesd_10', 'anxiety_9', 'anxiety_10', 'mood_9', 'mood_10',
-                         'employ_status_9', 'total_income_bu_d_9',
-                         'age_9', 'sex_9', 'ethnicity_9', 'marital_status_9',
-                         'memory_9', 'numeracy_9', 'edu_age_9', 'edu_qual_9', 'n_deprived_9']].dropna()
+sample_did = sample[['idauniq', 'PC1_b_9',
+                     'srh_9', 'srh_10',
+                     'high_bp_9', 'high_bp_10', 'high_chol_9', 'high_chol_10', 'diabetes_9', 'diabetes_10',
+                     'asthma_9', 'asthma_10', 'arthritis_9', 'arthritis_10', 'cancer_9', 'cancer_10',
+                     'cesd_9', 'cesd_10', 'anxiety_9', 'anxiety_10', 'mood_9', 'mood_10',
+                     'employ_status_9', 'total_income_bu_d_9',
+                     'age_9', 'sex_9', 'ethnicity_9', 'marital_status_9',
+                     'memory_9', 'numeracy_9', 'edu_age_9', 'edu_qual_9', 'n_deprived_9']].dropna()
 
-sample_did['group'].value_counts(dropna=False)  # 1: 535, 0: 619
+sample_did['PC1_b_9'].value_counts(dropna=False)  # 1: 555, 0: 281
 
 ####################
 # Another method of estimating propensity score
-logit_ps = smf.logit('group ~ total_income_bu_d_9 + employ_status_9 + age_9 + sex_9 + ethnicity_9 + C(marital_status_9) + memory_9 + numeracy_9 + edu_age_9 + C(edu_qual_9) + n_deprived_9',
-                     data=sample_did).fit()
+logit_ps = smf.logit(
+    'PC1_b_9 ~ total_income_bu_d_9 + employ_status_9 + age_9 + sex_9 + ethnicity_9 + C(marital_status_9) + memory_9 + numeracy_9 + edu_age_9 + C(edu_qual_9) + n_deprived_9',
+    data=sample_did).fit()
 
 sample_did['ps'] = logit_ps.predict(sample_did)
 
 # Perform matching
-# split the low and high group
-sample_did_low = sample_did.loc[sample_did['group'] == 0, :]
-sample_did_high = sample_did.loc[sample_did['group'] == 1, :]
+# split the low and high PC1_b_9
+sample_did_low = sample_did.loc[sample_did['PC1_b_9'] == 0, :]
+sample_did_high = sample_did.loc[sample_did['PC1_b_9'] == 1, :]
 
 # Nearest Neighbour Matching
 nn = NearestNeighbors(n_neighbors=1)
@@ -66,7 +67,8 @@ matches = pd.DataFrame({
 
 # Loop to get results
 did_result = pd.DataFrame({'Outcome': ['Self-rated health', '', 'High blood pressure', '', 'High cholesterol', '',
-                                       'Diabetes', '', 'Asthma', '', 'Arthritis', '', 'Cancer', '', 'Depression score', '',
+                                       'Diabetes', '', 'Asthma', '', 'Arthritis', '', 'Cancer', '', 'Depression score',
+                                       '',
                                        'Anxiety disorder', '', 'Mood swings', ''],
                            'DiD': np.nan,
                            'p_value': np.nan})
@@ -77,17 +79,19 @@ outcome_list = ['srh', 'high_bp', 'high_chol', 'diabetes', 'asthma', 'arthritis'
 for j in range(len(outcome_list)):
     did = []
     for i in range(len(matches)):
-        did.append((sample_did.loc[sample_did['idauniq'] == matches['high_ID'][i], f'{outcome_list[j]}_10'].values[0] - sample_did.loc[sample_did['idauniq'] == matches['high_ID'][i], f'{outcome_list[j]}_9'].values[0])
+        did.append((sample_did.loc[sample_did['idauniq'] == matches['high_ID'][i], f'{outcome_list[j]}_10'].values[0] -
+                    sample_did.loc[sample_did['idauniq'] == matches['high_ID'][i], f'{outcome_list[j]}_9'].values[0])
                    -
-                   (sample_did.loc[sample_did['idauniq'] == matches['low_ID'][i], f'{outcome_list[j]}_10'].values[0] - sample_did.loc[sample_did['idauniq'] == matches['low_ID'][i], f'{outcome_list[j]}_9'].values[0]))
+                   (sample_did.loc[sample_did['idauniq'] == matches['low_ID'][i], f'{outcome_list[j]}_10'].values[0] -
+                    sample_did.loc[sample_did['idauniq'] == matches['low_ID'][i], f'{outcome_list[j]}_9'].values[0]))
 
         # mean
-        did_result.iloc[j*2, 1] = np.mean(did)
+        did_result.iloc[j * 2, 1] = np.mean(did)
 
         # t-test
         t_stat, p_value = ttest_1samp(did, 0)
-        did_result.iloc[j*2, 2] = p_value
-        did_result.iloc[j*2 + 1, 1] = t_stat
+        did_result.iloc[j * 2, 2] = p_value
+        did_result.iloc[j * 2 + 1, 1] = t_stat
 
 ##### Format
 # Round to 3 decimal places
@@ -112,13 +116,9 @@ did_result['Description'] = [''] * did_result.shape[0]
 ########## LaTeX
 did_result[['Outcome', 'Description', 'DiD']].to_latex(index=False, float_format="%.3f") | p(print)
 
-
-
-
-
 ########## 5 Nearest Neighbours Matching
-sample_did_low = sample_did.loc[sample_did['group'] == 0, :]
-sample_did_high = sample_did.loc[sample_did['group'] == 1, :]
+sample_did_low = sample_did.loc[sample_did['PC1_b_9'] == 0, :]
+sample_did_high = sample_did.loc[sample_did['PC1_b_9'] == 1, :]
 
 # Nearest Neighbour Matching
 nn = NearestNeighbors(n_neighbors=5)
@@ -135,7 +135,8 @@ matches = pd.DataFrame({
 
 # Loop to get results
 did_result = pd.DataFrame({'Outcome': ['Self-rated health', '', 'High blood pressure', '', 'High cholesterol', '',
-                                       'Diabetes', '', 'Asthma', '', 'Arthritis', '', 'Cancer', '', 'Depressive symptoms', '',
+                                       'Diabetes', '', 'Asthma', '', 'Arthritis', '', 'Cancer', '',
+                                       'Depressive symptoms', '',
                                        'Anxiety disorder', '', 'Mood swings', ''],
                            'DiD': np.nan,
                            'p_value': np.nan})
@@ -146,17 +147,19 @@ outcome_list = ['srh', 'high_bp', 'high_chol', 'diabetes', 'asthma', 'arthritis'
 for j in range(len(outcome_list)):
     did = []
     for i in range(len(matches)):
-        did.append((sample_did.loc[sample_did['idauniq'] == matches['high_ID'][i], f'{outcome_list[j]}_10'].values[0] - sample_did.loc[sample_did['idauniq'] == matches['high_ID'][i], f'{outcome_list[j]}_9'].values[0])
+        did.append((sample_did.loc[sample_did['idauniq'] == matches['high_ID'][i], f'{outcome_list[j]}_10'].values[0] -
+                    sample_did.loc[sample_did['idauniq'] == matches['high_ID'][i], f'{outcome_list[j]}_9'].values[0])
                    -
-                   (sample_did.loc[sample_did['idauniq'] == matches['low_ID'][i], f'{outcome_list[j]}_10'].values[0] - sample_did.loc[sample_did['idauniq'] == matches['low_ID'][i], f'{outcome_list[j]}_9'].values[0]))
+                   (sample_did.loc[sample_did['idauniq'] == matches['low_ID'][i], f'{outcome_list[j]}_10'].values[0] -
+                    sample_did.loc[sample_did['idauniq'] == matches['low_ID'][i], f'{outcome_list[j]}_9'].values[0]))
 
         # mean
-        did_result.iloc[j*2, 1] = np.mean(did)
+        did_result.iloc[j * 2, 1] = np.mean(did)
 
         # t-test
         t_stat, p_value = ttest_1samp(did, 0)
-        did_result.iloc[j*2, 2] = p_value
-        did_result.iloc[j*2 + 1, 1] = t_stat
+        did_result.iloc[j * 2, 2] = p_value
+        did_result.iloc[j * 2 + 1, 1] = t_stat
 
 ##### Format
 # Round to 3 decimal places
@@ -181,10 +184,8 @@ did_result['Description'] = [''] * did_result.shape[0]
 ########## LaTeX
 did_result[['Outcome', 'Description', 'DiD']].to_latex(index=False, float_format="%.3f") | p(print)
 
-
-
 # ##### Estimate propensity score
-# psm = PsmPy(sample_did, treatment='group', indx='idauniq', exclude = ['srh_9', 'srh_10',
+# psm = PsmPy(sample_did, treatment='PC1_b_9', indx='idauniq', exclude = ['srh_9', 'srh_10',
 #                                                                       'high_bp_9', 'high_bp_10', 'high_chol_9', 'high_chol_10', 'diabetes_9', 'diabetes_10',
 #                                                                       'asthma_9', 'asthma_10', 'arthritis_9', 'arthritis_10', 'cancer_9', 'cancer_10',
 #                                                                       'cesd_9', 'cesd_10', 'anxiety_9', 'anxiety_10', 'mood_9', 'mood_10',
@@ -197,8 +198,8 @@ did_result[['Outcome', 'Description', 'DiD']].to_latex(index=False, float_format
 #
 # nmixx = psm.matched_ids  # first column: high; second column: low
 #
-# sample_did.loc[sample_did['idauniq'] == nmixx['idauniq'][0], 'group'].values[0]  # 1
-# sample_did.loc[sample_did['idauniq'] == nmixx['matched_ID'][0], 'group'].values[0]  # 0
+# sample_did.loc[sample_did['idauniq'] == nmixx['idauniq'][0], 'PC1_b_9'].values[0]  # 1
+# sample_did.loc[sample_did['idauniq'] == nmixx['matched_ID'][0], 'PC1_b_9'].values[0]  # 0
 #
 # ##### Calculate DiD
 # # create a dataframe to save the results
